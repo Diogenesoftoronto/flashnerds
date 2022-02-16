4a1e8b9f01d1c2891d7d3399718da9b5974c9249
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-15 19:48:56 -0500

    add sass

--- app/stories/index.js
+++ app/stories/index.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { Fragment } from 'react'
+
+import index.scss;
+
+import Button from "./Button"
+
+defaultValues = { 
+  text: "Hello World",
+  stuff1:" bye world"
+};
+
+storiesOf("Button", module)
+  .addParameter({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Default", () => <Button props={defaultValues} />)
+
+storiesOf("flashcard", module)
+  .addParameter({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Empty", () => {
+    <Flashcard />
+  })
