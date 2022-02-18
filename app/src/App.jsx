d987558e73c974d24c8865d8e0148c808d4faaac
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-18 18:48:49 -0500

    navbar link worked

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,36 +1,39 @@
 // import logo from './logo.svg';
-import {  BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from  './contexts/AuthContext'
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-// import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
 // import Logout from "./pages/Logout";
 
-// import MyLibrary from "./pages/MyLibrary";
-// import DeckCreate from "./pages/DeckCreate";
+import MyLibrary from "./pages/MyLibrary";
+import DeckCreate from "./pages/DeckCreate";
 // import DeckUpdate from "./pages/DeckUpdate";
 // import FlashcardDetails from "./pages/FlashcardDetails";
-// import FlashcardUpdate from ".pages/FlashcardUpdate";
+// import FlashcardUpdate from "./pages/FlashcardUpdate";
 // import UserUpdate from "./pages/UserUpdate";
 
-function App() {  return (
-  <Router>
-    <Navbar />
-  </Router>
+function App() {  
+  return (
+    <Router>
+        <Navbar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/mylibrary" element={ <MyLibrary /> } />
+        <Route path="/decks/create" element={ <DeckCreate /> } />
+      </Routes>
+    </Router>
   // <AuthProvider>
   //   <Router>
   //       <Routes>
-  //         {/* <Route path="/" element={ <Dashboard /> } /> */}
-
   //         {/* AuthRoutes */}
   //         {/* <Route path="/register" element={ <Register /> } />
   //         <Route path="/login" element={ <Login /> } />
   //         <Route path="/logout" element={ <Logout /> } />
 
-  //         <Route path="/mylibrary" element={ <MyLibrary /> } /> */}
 
   //         {/* flashRoutes */}
   //         {/* <Route path="/decks/:id" element={ <Dashboard /> } />
