9bd5110062d7ac26ce7fbbaa3bac18b23290ab77
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-17 20:43:54 -0500

    created pages folders and files, implement Navbar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {  BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import { AuthProvider } from  './contexts/AuthContext'
 import './App.css';
+import Search from './components/Navbar/Search';
+import Dashboard from "./pages/Dashboard";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+
+import MyLibrary from "./pages/MyLibrary";
+import DeckCreate from "./pages/DeckCreate";
+import DeckUpdate from "./pages/DeckUpdate";
+import FlashcardDetails from "./pages/FlashcardDetails";
+import FlashcardUpdate from ".pages/FlashcardUpdate";
+import UserUpdate from "./pages/UserUpdate";
 
 function App() {  return (
   <AuthProvider>
+    <Search />
     <Router>
         <Routes>
           <Route path="/" element={ <Dashboard /> } />
@@ -11,6 +27,8 @@ function App() {  return (
           <Route path="/register" element={ <Register /> } />
           <Route path="/login" element={ <Login /> } />
           <Route path="/logout" element={ <Logout /> } />
+
+          <Route path="/mylibrary" element={ <MyLibrary /> } />
 
           {/* flashRoutes */}
           <Route path="/decks/:id" element={ <Dashboard /> } />
---
+++ app/src/components/Navbar/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+ return (
+  <header>
+    <nav>
+      <div>Flashnerds</div>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/mylibrary">My Library</NavLink></li>
+        <li><NavLink to="/decks/create">New</NavLink></li>
+      </ul>
+    </nav>
+  </header>
+ )
+}
+
+export default Navbar
