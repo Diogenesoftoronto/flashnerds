c1430c408b5ae170331e2ae8eeac015dae066aaf
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-18 21:19:26 -0500

    add user helper

---
+++ server/db_helpers/userHelpers.js
@@ -0,0 +1,40 @@
+const getUserNameById = function(db, userInfo) {
+  let userValues = [userInfo];
+  let queryString = `SELECT name FROM users WHERE id = $1;`;
+  return db
+    .query(queryString, userValues)
+    .then(res => res.rows[0])
+    .catch(console.error("Error running query to get user name by id from database"));
+};
+
+const getUserProfileById = function(db, userInfo) {
+  let userValues = [userInfo.id];
+  let queryString = `SELECT name, description, image FROM users WHERE id = $1;`;
+  return db
+    .query(queryString, userValues)
+    .then(res => res.rows[0])
+    .catch(console.error("Error running query to get user profile by id from database"));
+};
+const getUserEmailNameAndPasswordById = function(db, userInfo) {
+  let userValues = [userInfo.id];
+  let queryString = `SELECT name, email, password FROM users WHERE id = $1;`;
+  return db
+    .query(queryString, userValues)
+    .then(res => res.rows[0])
+    .catch(console.error("Error running query to get user profile by id from database"));
+};
+
+const getAllUsers = function(db) {
+  return db
+    .query(`SELECT * FROM users;`)
+    .then(data => data.rows)
+    .catch(console.error("Error running query to get all users from database"));
+};
+const deleteFlashcard = function(db, flashcardInfo) {
+  let flashcardValues = [flashcardInfo.title,  flashcardInfo.flashcardId];
+  let queryString = `DELETE FROM flashcards WHERE id = $1::integer;`;
+  return db
+    .query(queryString, flashcardValues)
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+};
