8c4f5cfb8d5b02db712d80ab834c8da1a1752322
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-19 18:50:11 -0500

    decks implemented as a route

--- server/db_helpers/deckHelper.js
+++ server/db_helpers/deckHelper.js
@@ -1,47 +1,57 @@
-const getDeckByDeckID = function(db, deckId) {
+const getAllDecks = function (db) {
+  return db
+    .query(`SELECT * FROM decks;`)
+    .then((data) => data.rows)
+    .catch(console.error("Error running query to get all decks from database"));
+};
+
+const getDeckByDeckId = function (db, deckId) {
   let deckValues = [deckId];
   let queryString = `SELECT * FROM decks
                         WHERE decks.id = $1;`;
-  return (
-    db
-      .query(queryString, deckValues)
-      .then(res => {
-        return res.rows;
-      })
-      .catch(err => console.log(err))
-  );
+  return db
+    .query(queryString, deckValues)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => console.log(err));
 };
 
-const addDeck = function(db, deckInfo) {
-  let deckValues = [deckInfo.userId, deckInfo.name,  deckInfo.image];
+const addDeck = function (db, deckInfo) {
+  let deckValues = [deckInfo.userId, deckInfo.name, deckInfo.image];
   let queryString = `INSERT INTO decks (users_id, name, image)
   VALUES ($1,$2,$3) RETURNING *;`;
   return db
     .query(queryString, deckValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
-
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
 };
 
-const editDeck = function(db, deckInfo) {
-  let deckValues = [deckInfo.name,  deckInfo.userId, deckInfo.image, deckInfo.deckId];
+const editDeck = function (db, deckInfo) {
+  let deckValues = [
+    deckInfo.name,
+    deckInfo.userId,
+    deckInfo.image,
+    deckInfo.deckId,
+  ];
   let queryString = `UPDATE decks SET name = $1, users_id = $2, image = $3
   WHERE decks.id = $4 RETURNING *;`;
   return db
     .query(queryString, deckValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
 };
-const deleteDeck = function(db, deckId) {
+const deleteDeck = function (db, deckId) {
   let deckValues = [deckId];
   let queryString = `DELETE FROM decks WHERE id = $1;`;
   return db
     .query(queryString, deckValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
 };
 module.exports = {
-  getDeckByDeckID,
+  getAllDecks,
+  getDeckByDeckId,
   addDeck,
   editDeck,
   deleteDeck,
