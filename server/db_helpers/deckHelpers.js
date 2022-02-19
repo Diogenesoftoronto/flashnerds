6de403b9f0adcdad0696d2986b89d9818e04cb72
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-18 21:51:50 -0500

    change to proper names and add delete

--- server/db_helpers/deckHelpers.js
+++ server/db_helpers/deckHelpers.js
@@ -1,10 +1,10 @@
-const getFlashcardByMapId = function(db, mapID) {
-  let mapValues = [mapID];
-  let queryString = `SELECT * FROM maps
-                        WHERE maps.id = $1`;
+const getDeckByDeckID = function(db, deckIgetDeckBydeckID) {
+  let deckValues = [deckID];
+  let queryString = `SELECT * FROM decks
+                        WHERE decks.id = $1`;
   return (
     db
-      .query(queryString, mapValues)
+      .query(queryString, deckValues)
       .then(res => {
         return res.rows;
       })
@@ -12,31 +12,37 @@ const getFlashcardByMapId = function(db, mapID) {
   );
 };
 
-const addMap = function(db, mapInfo) {
-  let mapValues = [mapInfo.userId, mapInfo.title];
-  let queryString = `INSERT INTO maps (user_id, title)
+const addDeck = function(db, deckInfo) {
+  let deckValues = [deckInfo.userId, deckInfo.name];
+  let queryString = `INSERT INTO decks (user_id, name)
   VALUES ($1,$2) RETURNING *;`;
   return db
-    .query(queryString, mapValues)
+    .query(queryString, deckValues)
     .then(res => res.rows[0])
     .catch(err => console.log(err));
 
 };
 
-const editMap = function(db, mapInfo) {
-  let mapValues = [mapInfo.title,  mapInfo.mapId];
-  let queryString = `UPDATE maps SET title = $1
-  WHERE maps.id = $2 RETURNING *;`;
+const editDeck = function(db, deckInfo) {
+  let deckValues = [deckInfo.name,  deckInfo.deckIgetDeckBydeckID];
+  let queryString = `UPDATE decks SET name = $1
+  WHERE decks.id = $2 RETURNING *;`;
   return db
-    .query(queryString, mapValues)
+    .query(queryString, deckValues)
     .then(res => res.rows[0])
     .catch(err => console.log(err));
 };
-const deleteFlashcard = function(db, flashcardInfo) {
-  let flashcardValues = [flashcardInfo.title,  flashcardInfo.flashcardId];
-  let queryString = `DELETE FROM flashcards WHERE id = $1::integer;`;
+const deleteDeck = function(db, deckInfo) {
+  let deckValues = [deckInfo.name,  deckInfo.deckID];
+  let queryString = `DELETE FROM decks WHERE id = $1::integer;`;
   return db
-    .query(queryString, flashcardValues)
+    .query(queryString, deckValues)
     .then(res => res.rows[0])
     .catch(err => console.log(err));
+};
+module.exports = {
+  getDeckByDeckID,
+  addDeck,
+  editDeck,
+  deleteDeck,
 };
