e4726c14ffd3152c8037d9b2e05db38d244dfd79
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-19 17:46:29 -0500

    remove unnecessary imports and middleware from index

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const app = require('./app');
 const port = 3001
-const usersRoutes = require("./routes/users");
 
-app.use("/api/users", usersRoutes(db));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
