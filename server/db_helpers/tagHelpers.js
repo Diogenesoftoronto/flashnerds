2f2a683202493cc56d043428168a3a1dc0b07354
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-18 21:52:04 -0500

    change to proper names and add delete

--- server/db_helpers/tagHelpers.js
+++ server/db_helpers/tagHelpers.js
@@ -1,42 +1,45 @@
-const getTagByTagId = function(db, tagID) {
+const getTagByTagId = function (db, tagID) {
   let tagValues = [tagID];
   let queryString = `SELECT * FROM tags
                         WHERE tags.id = $1`;
-  return (
-    db
-      .query(queryString, tagValues)
-      .then(res => {
-        return res.rows;
-      })
-      .catch(err => console.log(err))
-  );
+  return db
+    .query(queryString, tagValues)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => console.log(err));
 };
 
-const addTag = function(db, tagInfo) {
+const addTag = function (db, tagInfo) {
   let tagValues = [tagInfo.flashcardId, tagInfo.name];
   let queryString = `INSERT INTO tags (flashcards_id, name)
   VALUES ($1,$2) RETURNING *;`;
   return db
     .query(queryString, tagValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
-
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
 };
 
-const editTag = function(db, tagInfo) {
-  let tagValues = [tagInfo.name,  tagInfo.tagId];
+const editTag = function (db, tagInfo) {
+  let tagValues = [tagInfo.name, tagInfo.tagId];
   let queryString = `UPDATE tags SET name = $1
   WHERE tags.id = $2 RETURNING *;`;
   return db
     .query(queryString, tagValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
 };
-const deleteFlashcard = function(db, flashcardInfo) {
-  let flashcardValues = [flashcardInfo.title,  flashcardInfo.flashcardId];
-  let queryString = `DELETE FROM flashcards WHERE id = $1::integer;`;
+const deleteTag = function (db, tagInfo) {
+  let tagValues = [tagInfo.title, tagInfo.tagId];
+  let queryString = `DELETE FROM tags WHERE id = $1::integer;`;
   return db
-    .query(queryString, flashcardValues)
-    .then(res => res.rows[0])
-    .catch(err => console.log(err));
+    .query(queryString, tagValues)
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
+};
+module.exports = {
+  getTagByTagId,
+  addTag,
+  editTag,
+  deleteTag,
 };
