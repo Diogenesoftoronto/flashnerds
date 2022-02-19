c665d98919bd8c966746b1393aef393d40e7704a
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-19 17:20:47 -0500

    remove pluralization for files, finish user routes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 // const app = express()
 const app = require('./app');
 const port = 3001
+const usersRoutes = require("./routes/users");
 
+app.use("/api/users", usersRoutes(db));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
