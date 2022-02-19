0a1026b69bdb3d11546b6a5fd9322d99cac682b2
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-18 21:19:41 -0500

    add comments route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,79 +1,77 @@
 //  all the routes for flashcards
 
-const router = require('express').Router();
+const router = require("express").Router();
+module.exports = (db) => {
+  router.get("/flashcards", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
 
-router.get("/api/flashcards", (request, response) => {
-  if (process.env.TEST_ERROR) {
-    setTimeout(() => response.status(500).json({}), 1000);
-    return;
-  }
-
-  db.query(`SELECT * FROM flashcards WHERE decks_id = $1`, [
-    request.params.id
-  ]).then(() => {
-    setTimeout(() => {
-      response.status(204).json({});
-      updateAppointment(Number(request.params.id), null);
-    }, 1000);
+    db.query(`SELECT * FROM flashcards WHERE decks_id = $1`, [
+      request.params.id,
+    ]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+        updateFlashcards(Number(request.params.id), null);
+      }, 1000);
+    });
   });
-});
-router.get("/api/flashcards/:id", (request, response) => {
-  if (process.env.TEST_ERROR) {
-    setTimeout(() => response.status(500).json({}), 1000);
-    return;
-  }
+  router.get("/flashcards/:id", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
 
-  db.query(`SELECT * FROM flashcards WHERE id = $1`, [
-    request.params.id
-  ]).then(() => {
-    setTimeout(() => {
-      response.status(204).json({});
-      updateAppointment(Number(request.params.id), null);
-    }, 1000);
+    db.query(`SELECT * FROM flashcards WHERE id = $1`, [
+      request.params.id,
+    ]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+        updateFlashcards(Number(request.params.id), null);
+      }, 1000);
+    });
   });
-});
-router.put("/api/flashcards/:id", (request, response) => {
-  if (process.env.TEST_ERROR) {
-    setTimeout(() => response.status(500).json({}), 1000);
-    return;
-  }
+  router.put("/flashcards/:id", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
 
-  db.query(`UPDATE  WHERE id = $1::integer`, [
-    request.params.id
-  ]).then(() => {
-    setTimeout(() => {
-      response.status(204).json({});
-      updateAppointment(Number(request.params.id), null);
-    }, 1000);
+    db.query(`UPDATE  WHERE id = $1::integer`, [request.params.id]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+        updateFlashcards(Number(request.params.id), null);
+      }, 1000);
+    });
   });
-});
-router.post("/api/flashcards/:id", (request, response) => {
-  if (process.env.TEST_ERROR) {
-    setTimeout(() => response.status(500).json({}), 1000);
-    return;
-  }
+  router.post("/flashcards/:id", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
 
-  db.query(`DELETE FROM interviews WHERE appointment_id = $1::integer`, [
-    request.params.id
-  ]).then(() => {
-    setTimeout(() => {
-      response.status(204).json({});
-      updateAppointment(Number(request.params.id), null);
-    }, 1000);
+    db.query(`DELETE FROM interviews WHERE flashcards_id = $1::integer`, [
+      request.params.id,
+    ]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+        updateFlashcards(Number(request.params.id), null);
+      }, 1000);
+    });
   });
-});
-router.delete("/api/flashcards/:id", (request, response) => {
-  if (process.env.TEST_ERROR) {
-    setTimeout(() => response.status(500).json({}), 1000);
-    return;
-  }
+  router.delete("/flashcards/:id", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
 
-  db.query(`DELETE FROM flashcards WHERE id = $1::integer`, [
-    request.params.id
-  ]).then(() => {
-    setTimeout(() => {
-      response.status(204).json({});
-      updateAppointment(Number(request.params.id), null);
-    }, 1000);
+    db.query(`DELETE FROM flashcards WHERE id = $1::integer`, [
+      request.params.id,
+    ]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+        updateFlashcards(Number(request.params.id), null);
+      }, 1000);
+    });
   });
-});
