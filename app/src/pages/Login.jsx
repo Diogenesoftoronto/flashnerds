f34bab1954fa40bed0e92d482d6f62fd38767650
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-19 12:47:48 -0500

    make login form worked

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,13 +1,25 @@
 import "./Login.scss";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from '../contexts/AuthContext';
 
 function Login(props) {
   const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("login!");
-    navigate("/home");
+    console.log(`${userName}, ${password}`);
+    login(userName, password).then(response => {
+        console.log(response);
+        alert("login!");
+        navigate("/home");
+    }).catch(e => {
+        console.error(e); alert('login failed');
+    });
   };
 
   return (
@@ -15,10 +27,14 @@ function Login(props) {
       <img src="/books.png" alt="logo" />
       <form onSubmit={handleSubmit} method="post">
         <div>
-          <input type="text" name="" id="" />
+          <input type="text" name="" id="" 
+            defaultValue={userName} 
+            onChange={(e) => setUserName(e.currentTarget.value)} />
         </div>
         <div>
-          <input type="password" name="" id="" />
+          <input type="password" name="" id="" 
+            defaultValue={password} 
+            onChange={(e) => setPassword(e.currentTarget.value)} />
         </div>
         <button type="submit">Login</button>
       </form>
