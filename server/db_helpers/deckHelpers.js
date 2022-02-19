52163a3fbdc1218fdad2c51572797e5c121e8ee1
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-18 21:18:58 -0500

    add deck helper

---
+++ server/db_helpers/deckHelpers.js
@@ -0,0 +1,42 @@
+const getFlashcardByMapId = function(db, mapID) {
+  let mapValues = [mapID];
+  let queryString = `SELECT * FROM maps
+                        WHERE maps.id = $1`;
+  return (
+    db
+      .query(queryString, mapValues)
+      .then(res => {
+        return res.rows;
+      })
+      .catch(err => console.log(err))
+  );
+};
+
+const addMap = function(db, mapInfo) {
+  let mapValues = [mapInfo.userId, mapInfo.title];
+  let queryString = `INSERT INTO maps (user_id, title)
+  VALUES ($1,$2) RETURNING *;`;
+  return db
+    .query(queryString, mapValues)
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+
+};
+
+const editMap = function(db, mapInfo) {
+  let mapValues = [mapInfo.title,  mapInfo.mapId];
+  let queryString = `UPDATE maps SET title = $1
+  WHERE maps.id = $2 RETURNING *;`;
+  return db
+    .query(queryString, mapValues)
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+};
+const deleteFlashcard = function(db, flashcardInfo) {
+  let flashcardValues = [flashcardInfo.title,  flashcardInfo.flashcardId];
+  let queryString = `DELETE FROM flashcards WHERE id = $1::integer;`;
+  return db
+    .query(queryString, flashcardValues)
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+};
