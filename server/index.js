0f82f85c5ed8ec40fc0df8d81081655255def739
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-19 14:15:11 -0500

    make server side api work

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const app = express()
+// const app = express()
+const app = require('./app');
 const port = 3001
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
