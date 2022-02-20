27d0cba8674392c9db872f4131c8ae3c4b95ed0f
TJ <michael.btj@hotmail.com>
2022-02-20 17:26:22 -0500

    bugfix on styled

--- app/src/components/Comments.jsx
+++ app/src/components/Comments.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { styled, keyframes } from '@stitches/react';
 import { violet, blackA } from '@radix-ui/colors';
-import { RowSpacingIcon, Cross2Icon } from '@radix-ui/react-icons';
+import { RowSpacingIcon, Cross2Icon } from '@modulz/radix-icons';
 import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
 
-const StyledCollapsible = styled(CollapsiblePrimitive.Root, {
-  width: 300,
-});
 
-// Exports
-export const Collapsible = StyledCollapsible;
-export const CollapsibleTrigger = CollapsiblePrimitive.Trigger;
-export const CollapsibleContent = styled(CollapsiblePrimitive.Content, {
-  overflow: 'hidden',
-  '&[data-state="open"]': { animation: `${open} 300ms ease-out` },
-  '&[data-state="closed"]': { animation: `${close} 300ms ease-out` },
-});
+const StyledCollapsible = CollapsiblePrimitive.Root;
 
 // transition 
 const open = keyframes({
@@ -27,7 +17,18 @@ const close = keyframes({
   from: { height: 'var(--radix-collapsible-content-height)' },
   to: { height: 0 },
 });
-// Your app...
+
+
+// Exports
+export const Collapsible = StyledCollapsible;
+export const CollapsibleTrigger = CollapsiblePrimitive.Trigger;
+export const CollapsibleContent = styled(CollapsiblePrimitive.Content, {
+  overflow: 'hidden',
+  '&[data-state="open"]': { animation: `${open} 300ms ease-out` },
+  '&[data-state="closed"]': { animation: `${close} 300ms ease-out` },
+});
+
+
 const Flex = styled('div', { display: 'flex' });
 const Text = styled('span', {
   color: violet.violet11,
@@ -61,12 +62,13 @@ const Comment = styled('div', {
   boxShadow: `0 2px 10px ${blackA.blackA7}`,
 });
 
-export const Comments = () => {
+const Comments = () => {
   const [open, setOpen] = React.useState(false);
   return (
     <Collapsible open={open} onOpenChange={setOpen}>
       <Flex css={{ alignItems: 'center', justifyContent: 'space-between' }}>
-        <Text css={{ color: 'white' }}>@peduarte starred 3 repositories</Text>
+        {/* have a form for writing a comment */}
+        <Text css={{ color: 'white' }}>View Comments</Text>
         <CollapsibleTrigger asChild>
           <IconButton>{open ? <Cross2Icon /> : <RowSpacingIcon />}</IconButton>
         </CollapsibleTrigger>
