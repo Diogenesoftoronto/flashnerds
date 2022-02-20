600e7eecbf710741e3a3b84479c118d2b2462fcd
TJ <michael.btj@hotmail.com>
2022-02-19 22:51:01 -0500

    add flashcard story

---
+++ app/src/stories/Flashcard.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Flashcard from '../components/Flashcard';
+
+export default {
+  title: 'Flashcard',
+  component: Flashcard
+}
+
+export const Primary = () => <Flashcard question='Question' answer='Answer'/>;
+
