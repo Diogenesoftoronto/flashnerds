33818eef66d552d3fabcda8467db1f7f3d02175f
Diogenesoftoronto <kmannoel@gmail.com>
2022-02-19 22:22:44 -0500

    format and check if user exists in auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const verifyToken = require("../middlewares/verifyToken");
 const SECRET = "test";
+const userRoutes = require("./routes/users");
+const axios = require('axios')
+module.exports = (userInfo) => {
+  router.post("/login", (request, response) => {
+      const found = await axios.get("/api/users/find", {params: {userInfo}})
+        // if user is not found then return "user not found, please register"
+        if (!found.data)
+        return response.json({ message: "user not found, please register" });
 
-module.exports = () => {
-    router.post('/auth/login', (request, response) => {
-        const token = jwt.sign({id:1}, SECRET, { expiresIn: '10m'});
-        response.json({token, message: 'test'});
-    });
+        const token = jwt.sign({ id: 1 }, SECRET, { expiresIn: "10m" });
+        response.json({ token, message: "test" });
+  });
+  router.post("/test", verifyToken, (request, response) => {
+    response.json({ message: "u r authorized" });
+  });
 
-    router.post('/auth/test', verifyToken, (request, response) => {
-        response.json({message: 'u r authorized'});
-    });
-
-
-    return router;
+  return router;
 };
