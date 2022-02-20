32675bed6507ee9129a953b36eac48e807ad6b99
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-19 23:00:52 -0500

    able to uplode image

--- app/src/pages/DeckCreate.jsx
+++ app/src/pages/DeckCreate.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { useState } from "react";
 
 function DeckCreate () {
+  const [imageUrl, setImageUrl] = useState("");
+  const cloud_name = "dbbnv85af";
+  const upload_preset = "daxonv2q";
+  const handleClick = () => {
+    const { files } = document.querySelector(".uploadInput");
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("upload_preset", upload_preset);
+    const options = {
+     method: "POST",
+     body: formData,
+    };
+    return fetch(`https://api.Cloudinary.com/v1_1/${cloud_name}/image/upload`, options
+   )
+    .then((res) => res.json())
+    .then((res) => {
+      setImageUrl(res.secure_url);
+     })
+     .catch((err) => console.log(err));
+   };
+
   return (
     <div>
       <h1>Create New Deck</h1>
@@ -8,9 +30,10 @@ function DeckCreate () {
         <label for="deckTitle">Enter Title</label>
         <input type="text" name="deckTitle" id="deckTitel" />
       </div>
-      <div>
-        <input type="file" className="app_uploadInput" />
-        <button className="app_uploadButton">Upload</button>
+      <div className="imageUpload">
+        <input type="file" className="uploadInput" />
+        <img src={imageUrl} className="uploadedImg" alt="" />
+        <button className="uploadButton" onClick={handleClick}>Upload</button>
       </div>
     </div>
   )
