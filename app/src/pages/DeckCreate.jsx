7e87197677051bbfe065ebac8665ec2a81213e86
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-19 22:13:19 -0500

    image uplode area

--- app/src/pages/DeckCreate.jsx
+++ app/src/pages/DeckCreate.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 function DeckCreate () {
   return (
     <div>
-      Create New Deck
+      <h1>Create New Deck</h1>
+      <div>
+        <label for="deckTitle">Enter Title</label>
+        <input type="text" name="deckTitle" id="deckTitel" />
+      </div>
+      <div>
+        <input type="file" className="app_uploadInput" />
+        <button className="app_uploadButton">Upload</button>
+      </div>
     </div>
   )
 }
