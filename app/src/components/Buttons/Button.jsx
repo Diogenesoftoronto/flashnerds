8881701799681af73262f3b88917be983ad3a22c
TJ <michael.btj@hotmail.com>
2022-02-21 15:48:32 -0500

    add button component

---
+++ app/src/components/Buttons/Button.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { styled } from "@stitches/react";
+import { violet, blackA, mauve, green, red } from "@radix-ui/colors";
+
+
+/** 
+ Button props:
+ variant: default violet
+
+ this controls the style of the button
+
+ example: <Button variant="red">Delete</Button>
+  */
+
+export const Button = styled("button", {
+  all: "unset",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 4,
+  padding: "0 15px",
+  fontSize: 15,
+  lineHeight: 1,
+  fontWeight: 500,
+  height: 35,
+
+  variants: {
+    variant: {
+      violet: {
+        backgroundColor: "white",
+        color: violet.violet11,
+        boxShadow: `0 2px 10px ${blackA.blackA7}`,
+        "&:hover": { backgroundColor: mauve.mauve3 },
+        "&:focus": { boxShadow: `0 0 0 2px black` }
+      },
+      red: {
+        backgroundColor: red.red4,
+        color: red.red11,
+        '&:hover': { backgroundColor: red.red5 },
+        '&:focus': { boxShadow: `0 0 0 2px ${red.red7}` },
+      },
+      green: {
+        backgroundColor: green.green4,
+        color: green.green11,
+        "&:hover": { backgroundColor: green.green5 },
+        "&:focus": { boxShadow: `0 0 0 2px ${green.green7}` }
+      },
+      mauve: {
+        backgroundColor: mauve.mauve4,
+        color: mauve.mauve11,
+        '&:hover': { backgroundColor: mauve.mauve5 },
+        '&:focus': { boxShadow: `0 0 0 2px ${mauve.mauve7}` }
+      }
+    },
+
+    defaultVariants: {
+      variant: "violet"
+    }
+  }
+})
