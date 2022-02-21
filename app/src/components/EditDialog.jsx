4c9b900857fc918f967eb893d6a42453eac63726
TJ <michael.btj@hotmail.com>
2022-02-21 14:47:06 -0500

    edit dialog is now state based

--- app/src/components/EditDialog.jsx
+++ app/src/components/EditDialog.jsx
@@ -1,51 +1,52 @@
-import React from 'react';
-import { styled, keyframes } from '@stitches/react';
-import { violet, blackA, mauve, green } from '@radix-ui/colors';
-import { Cross2Icon } from '@radix-ui/react-icons';
-import * as DialogPrimitive from '@radix-ui/react-dialog';
-import { useState } from 'react';
+import React from "react";
+import { styled, keyframes } from "@stitches/react";
+import { violet, blackA, mauve, green } from "@radix-ui/colors";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { useState } from "react";
 
 const overlayShow = keyframes({
-  '0%': { opacity: 0 },
-  '100%': { opacity: 1 },
+  "0%": { opacity: 0 },
+  "100%": { opacity: 1 }
 });
 
 const contentShow = keyframes({
-  '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
-  '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+  "0%": { opacity: 0, transform: "translate(-50%, -48%) scale(.96)" },
+  "100%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" }
 });
 
 const StyledOverlay = styled(DialogPrimitive.Overlay, {
   backgroundColor: blackA.blackA9,
-  position: 'fixed',
+  position: "fixed",
   inset: 0,
-  '@media (prefers-reduced-motion: no-preference)': {
-    animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  },
+  "@media (prefers-reduced-motion: no-preference)": {
+    animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`
+  }
 });
 
 const StyledContent = styled(DialogPrimitive.Content, {
-  backgroundColor: 'white',
+  backgroundColor: "white",
   borderRadius: 6,
-  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '90vw',
-  maxWidth: '450px',
-  maxHeight: '85vh',
+  boxShadow:
+    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90vw",
+  maxWidth: "450px",
+  maxHeight: "85vh",
   padding: 25,
-  '@media (prefers-reduced-motion: no-preference)': {
-    animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  "@media (prefers-reduced-motion: no-preference)": {
+    animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`
   },
-  '&:focus': { outline: 'none' },
+  "&:focus": { outline: "none" }
 });
 
 function Content({ children, ...props }) {
   return (
     <DialogPrimitive.Portal>
-      <StyledOverlay  />
+      <StyledOverlay />
       <StyledContent {...props}>{children}</StyledContent>
     </DialogPrimitive.Portal>
   );
@@ -55,14 +56,14 @@ const StyledTitle = styled(DialogPrimitive.Title, {
   margin: 0,
   fontWeight: 500,
   color: mauve.mauve12,
-  fontSize: 17,
+  fontSize: 17
 });
 
 const StyledDescription = styled(DialogPrimitive.Description, {
-  margin: '10px 0 20px',
+  margin: "10px 0 20px",
   color: mauve.mauve11,
   fontSize: 15,
-  lineHeight: 1.5,
+  lineHeight: 1.5
 });
 
 // Exports
@@ -74,16 +75,16 @@ const DialogDescription = StyledDescription;
 // const DialogClose = DialogPrimitive.Close;
 
 // Your app...
-const Flex = styled('div', { display: 'flex' });
-const Box = styled('div', {});
+const Flex = styled("div", { display: "flex" });
+const Box = styled("div", {});
 
-const Button = styled('button', {
-  all: 'unset',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+const Button = styled("button", {
+  all: "unset",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
   borderRadius: 4,
-  padding: '0 15px',
+  padding: "0 15px",
   fontSize: 15,
   lineHeight: 1,
   fontWeight: 500,
@@ -92,133 +93,127 @@ const Button = styled('button', {
   variants: {
     variant: {
       violet: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         color: violet.violet11,
         boxShadow: `0 2px 10px ${blackA.blackA7}`,
-        '&:hover': { backgroundColor: mauve.mauve3 },
-        '&:focus': { boxShadow: `0 0 0 2px black` },
+        "&:hover": { backgroundColor: mauve.mauve3 },
+        "&:focus": { boxShadow: `0 0 0 2px black` }
       },
       green: {
         backgroundColor: green.green4,
         color: green.green11,
-        '&:hover': { backgroundColor: green.green5 },
-        '&:focus': { boxShadow: `0 0 0 2px ${green.green7}` },
-      },
-    },
+        "&:hover": { backgroundColor: green.green5 },
+        "&:focus": { boxShadow: `0 0 0 2px ${green.green7}` }
+      }
+    }
   },
 
   defaultVariants: {
-    variant: 'violet',
-  },
+    variant: "violet"
+  }
 });
 
-const IconButton = styled('button', {
-  all: 'unset',
-  fontFamily: 'inherit',
-  borderRadius: '100%',
+const IconButton = styled("button", {
+  all: "unset",
+  fontFamily: "inherit",
+  borderRadius: "100%",
   height: 25,
   width: 25,
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
   color: violet.violet11,
-  position: 'absolute',
+  position: "absolute",
   top: 10,
   right: 10,
 
-  '&:hover': { backgroundColor: violet.violet4 },
-  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet7}` },
+  "&:hover": { backgroundColor: violet.violet4 },
+  "&:focus": { boxShadow: `0 0 0 2px ${violet.violet7}` }
 });
 
-const Form = styled('form', {
-  all: 'unset',
-  display: 'flex',
+const Form = styled("form", {
+  all: "unset",
+  display: "flex",
   gap: 20,
-  alignItems: 'center',
-  marginBottom: 15,
+  alignItems: "center",
+  marginBottom: 15
 });
 
-const Label = styled('label', {
+const Label = styled("label", {
   fontSize: 15,
   color: violet.violet11,
   width: 90,
-  textAlign: 'right',
+  textAlign: "right"
 });
 
-const Input = styled('input', {
-  all: 'unset',
-  width: '100%',
-  flex: '1',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+const Input = styled("input", {
+  all: "unset",
+  width: "100%",
+  flex: "1",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
   borderRadius: 4,
-  padding: '0 10px',
+  padding: "0 10px",
   fontSize: 15,
   lineHeight: 1,
   color: violet.violet11,
   boxShadow: `0 0 0 1px ${violet.violet7}`,
   height: 35,
 
-  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet8}` },
+  "&:focus": { boxShadow: `0 0 0 2px ${violet.violet8}` }
 });
 
 const EditDialog = (props) => {
-
-  const { content, onSubmit} = props;
-
-  // Controls open and close of dialog popup
-  const [open, onOpenChange] = useState(false);
-
+  
+  const { state, setState, content, onSubmit } = props;
 
   const [value, setValue] = useState(content);
 
-  const toggleDialog = () => onOpenChange(!open);
 
   const cancel = () => {
-    onOpenChange(false);
-  }
+    setState("DEFAULT");
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(value);
-  }
+  };
 
-return  (
-  <Dialog open={open ? 'open' : null} >
-    {/* <DialogTrigger asChild> */}
-      <Button onClick={toggleDialog} size="large">Edit profile</Button>
-    {/* </DialogTrigger> */}
-    <DialogContent >
-      <DialogTitle>Edit</DialogTitle>
-      <DialogDescription>
-        Make changes to your comment
-      </DialogDescription>
-      <Form>
-        <Label htmlFor="comment">Edit</Label>
-        <Input
-        id="comment-stuff"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        />
-      </Form>
+  return (
+    <Dialog open={state === "EDIT" ? "open" : null}>
+      {/* <DialogTrigger asChild> */}
+      <Button onClick={() => setState("EDIT")} size="large">
+        Edit Comment
+      </Button>
+      {/* </DialogTrigger> */}
+      <DialogContent>
+        <DialogTitle>Edit</DialogTitle>
+        <DialogDescription>Make changes to your comment</DialogDescription>
+        <Form>
+          <Label htmlFor="comment">Edit</Label>
+          <Input
+            id="comment-stuff"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </Form>
 
-      <Flex css={{ marginTop: 25, justifyContent: 'flex-end' }}>
-        {/* <DialogClose asChild> */}
+        <Flex css={{ marginTop: 25, justifyContent: "flex-end" }}>
+          {/* <DialogClose asChild> */}
           <Button onClick={handleSubmit} aria-label="Close" variant="green">
             Save changes
           </Button>
-        {/* </DialogClose> */}
-      </Flex>
-      {/* <DialogClose asChild> */}
+          {/* </DialogClose> */}
+        </Flex>
+        {/* <DialogClose asChild> */}
         <IconButton onClick={cancel}>
           <Cross2Icon />
         </IconButton>
-      {/* </DialogClose> */}
-    </DialogContent>
-  </Dialog>
-);
-}
+        {/* </DialogClose> */}
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default EditDialog;
-
