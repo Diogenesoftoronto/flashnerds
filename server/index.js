9c699e63de8f3ff52f206e2c9e194061b6ed9956
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-21 15:49:34 -0500

    modularize and implement token verification

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.DB_PORT || 3001;
+const port = process.env.DB_PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
