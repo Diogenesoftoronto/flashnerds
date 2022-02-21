a278125f809ad6abbbeb7f7254d2a5c644d0d619
TJ <michael.btj@hotmail.com>
2022-02-21 00:42:38 -0500

    fix stories

--- app/src/stories/EditDeleteMenu.stories.jsx
+++ app/src/stories/EditDeleteMenu.stories.jsx
@@ -3,11 +3,23 @@ import EditDeleteMenu from "../components/EditDeleteMenu";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 
+export default {
+  title: "EditDeleteMenu",
+  component: EditDeleteMenu
+}
 
-storiesOf("DropdownMenu", module)
-.add('User owns comment', () => {
-  <EditDeleteMenu onEdit={action("onEdit")} onDelete={action("onDelete")}/>;
-})
-.add('User does not own comment', () => {
-  <EditDeleteMenu disabled onEdit={action("onEdit")} onDelete={action("onDelete")}/>;
-})
+export const Egg = () => <EditDeleteMenu onEdit={action("onEdit")} onDelete={action("onDelete")}/>;
+
+export const Bacon = () => <EditDeleteMenu disabled onEdit={action("onEdit")} onDelete={action("onDelete")}/>;
+
+
+// storiesOf("EditDeleteMenu", module)
+//   .addParameters({
+
+//   })
+//   .add('User owns comment', () => {
+//     <EditDeleteMenu onEdit={action("onEdit")} onDelete={action("onDelete")} />;
+//   })
+//   .add('User does not own comment', () => {
+//     <EditDeleteMenu disabled onEdit={action("onEdit")} onDelete={action("onDelete")} />;
+//   })
