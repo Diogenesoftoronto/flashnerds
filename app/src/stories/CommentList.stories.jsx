58170e0bc6bd47eafe58daea84c46efc7dda13a6
TJ <michael.btj@hotmail.com>
2022-02-21 19:48:42 -0500

    update stories

--- app/src/stories/CommentList.stories.jsx
+++ app/src/stories/CommentList.stories.jsx
@@ -6,4 +6,23 @@ export default {
   component: CommentList
 }
 
-export const Primary = () => <CommentList/>
+const comments = [
+  {
+    id: 1,
+    avatar: "https://i.imgur.com/LpaY82x.png",
+    userName: "Bobby",
+    content: "This is a comment.",
+    likes: 20,
+    timestamp: 1645483654553
+  },
+  {
+    id: 2,
+    avatar: "https://i.imgur.com/LpaY82x.png",
+    userName: "Bobby's Twin",
+    content: "This is another comment.",
+    likes: 200,
+    timestamp: 1645483650000
+  }
+];
+
+export const Primary = () => <CommentList comments={comments}/>
