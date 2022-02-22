38a6dfa9b8e186c6eecce45910139b1c11eed745
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-22 03:25:08 -0500

    this file is kind of redundant may delete later

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import "components/Button.scss";
 
-function Button(props) {
+/** 
+ Button props:
+ variant: default violet(not working though)
+
+ this controls the style of the button
+
+ example: <Button variant="red">Delete</Button>
+  */
+
+
+function Button (props) {
   const { color, label, action} = props;
    return (
       <button onClick={action} style={{color: color}}>
