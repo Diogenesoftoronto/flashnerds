5c90449fe8c752856dc11daed0aa7be666ea4420
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-22 18:15:30 -0500

    create answerform with stlyes

---
+++ app/src/components/AnswerForm.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import { styled } from '@stitches/react'
+import { Pencil2Icon } from '@radix-ui/react-icons';
+
+export const tbd = styled("div", {})
+
+export const AnswerForm = () => {
+  const [answerState, setAnswerState] = useState('');
+  return(
+  <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+    <input type="text" name="answer" placeholder="What's your guess nerd?"
+    onChange={event => setAnswerState(event.target.value)}
+    vale={answerState} />
+  </form>
+  ) 
+} 
