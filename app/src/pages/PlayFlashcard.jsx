92daa1cb751e9b12559f1f3784a1f5773f3827e4
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-22 19:01:30 -0500

    created playflashcard.jsx file

---
+++ app/src/pages/PlayFlashcard.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import FlashcardPost from '../components/FlashcardPost';
+import { useParams } from "react-router-dom";
+
+const comments = [
+  {
+    id: 1,
+    avatar: "https://i.imgur.com/LpaY82x.png",
+    userName: "Bobby",
+    content: "This is a comment.",
+    likes: 20,
+    timestamp: 1645483654553
+  },
+  {
+    id: 2,
+    avatar: "https://i.imgur.com/LpaY82x.png",
+    userName: "Bobby's Twin",
+    content: "This is another comment.",
+    likes: 200,
+    timestamp: 1645483650000
+  }
+];
+
+const question = "Question";
+const answer = "Answer";
+const likes = 0;
+
+
+function PlayFlashcard() {
+  const { id } = useParams();
+  console.log("hey", id);
+  // const { currentUser } = useAuth();
+
+  // return (
+  //   <div>
+  //     Home: {currentUser.name}
+  //   </div>
+  // )
+
+
+
+
+  return (
+    <div>
+      <FlashcardPost
+        question={question}
+        answer={answer}
+        likes={likes}
+        onBack={() => alert("onBack")}
+        onNext={() => alert("onNext")}
+        comments={comments}
+        />
+    </div>
+  );
+
+}
+
+export default PlayFlashcard
