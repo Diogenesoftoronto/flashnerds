8ca4e7e01835b81697baec087eed9dd126331b25
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-23 11:53:12 -0500

    render green or red depending on correct or incorrect

--- app/src/components/Flashcard.jsx
+++ app/src/components/Flashcard.jsx
@@ -1,55 +1,101 @@
 import { React, useState } from "react";
-import ReactCardFlip from 'react-card-flip';
+import ReactCardFlip from "react-card-flip";
 import { Button } from "./Buttons/Button";
-import classNames from 'classnames';
+import classNames from "classnames";
 
-import './styles/Flashcard.scss';
+import "./styles/Flashcard.scss";
 import { AnswerForm } from "./AnswerForm";
+import { styled } from "../stitches.config";
+
+
 
 function Flashcard(props) {
   const { question, answer } = props;
   
   const [isFlipped, setIsFlipped] = useState(false);
+  const [answerState, setAnswerState] = useState("");
+  function findAnswer(userAnswer, answer){
+    const regex = `/\b(${userAnswer})\b/g`
+    const found = answer.match(regex)
+    const arrResults = [...found]
+    if (!arrResults[0]) return false
+    return true
+  }
 
   const flashcardClass = classNames("flashcard-container", {
-    "flipped": isFlipped 
+    flipped: isFlipped,
   });
-
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
-  }
+  };
   // $(".flippable").click(function(){
   //   $(this).toggleClass("flipme");
   // });
+  const Back = styled("div", {
+    variant: {
+      variant: {
+        correct: {
+          backgroundColor: "green",
+          },
+        incorrect: {
+          backgroundColor: "red",
+          }
+        }
+     }
+});
+
+  const flashcardBack = (findAnswer(answerState, answer) ?  <Back variant="correct" className="flashcard back">
+  <header>
+    <h2>Answer</h2>
+  </header>
+  <span>{answer}</span>
+  <Button variant={"mauve"} onClick={handleClick}>
+    Flip
+  </Button>
+</Back> : (<Back variant="incorrect" className="flashcard back">
+  <header>
+    <h2>Answer</h2>
+    </header>
+    <span>{answer}</span>
+    <Button variant={"mauve"} onClick={handleClick}>
+      Flip
+      </Button>
+      </Back>
+      ));
   return (
-    
     // <div className={flashcardClass}>
-      <ReactCardFlip className="flashcard-container" isFlipped={isFlipped} flipDirection="horizontal">
+    <ReactCardFlip
+      className="flashcard-container"
+      isFlipped={isFlipped}
+      flipDirection="horizontal"
+    >
+      {/* do not use id and remember to use className and not class*/}
 
-{/* do not use id and remember to use className and not class*/}
-
-        <div className="flashcard front">
-          <header><h2>Question</h2></header>
-          <span>
+      <div className="flashcard front">
+        <header>
+          <h2>Question</h2>
+        </header>
+        <span>
           {question}
-          <AnswerForm />
-          </span>
-          {/* <img src="./flip-arrow.png" alt="Flip Arrow" onClick={handleClick} /> */}
-          <Button variant={'mauve'} onClick={handleClick} >Flip</Button>
-        </div>
+          <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+            <input
+              type="text"
+              name="answer"
+              placeholder="What's your guess nerd?"
+              onChange={(event) => setAnswerState(event.target.value)}
+              value={answerState}
+            />
+          </form>
+        </span>
+        {/* <img src="./flip-arrow.png" alt="Flip Arrow" onClick={handleClick} /> */}
+        <Button variant={"mauve"} onClick={handleClick}>
+          Flip
+        </Button>
+      </div>
 
-        <div className="flashcard back">
-        <header><h2>Answer</h2></header>
-          <span>
-          {answer}
-          </span>
-          {/* instead of flip arrow png change to svg or have button that changes state */}
-          {/* <img src="./flip-arrow.png" alt="Flip arrow" onClick={handleClick} /> */}
-          <Button variant={'mauve'} onClick={handleClick} >Flip</Button>
-        </div>
-
-      </ReactCardFlip>
+    {flashcardBack}
+    </ReactCardFlip>
     //{/* </div> */}
   );
 }
