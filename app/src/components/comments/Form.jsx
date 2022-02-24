73aa1a6fb0f24b9efe3a4a5bb4a2121e72b5db06
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-23 22:59:32 -0500

    reformat

--- app/src/components/comments/Form.jsx
+++ app/src/components/comments/Form.jsx
@@ -3,27 +3,27 @@ import { Button } from "./altButton";
 import { styled } from "@stitches/react";
 // could add font family and other styles here. decided to make it simple
 
-const Comment = styled('div', {
+const Comment = styled("div", {
   borderRadius: 4,
-  margin: '10px 0',
+  margin: "10px 0",
   padding: 10,
   variants: {
     variant: {
       primary: {
         backgroundColor: "#0a9396",
         color: "#e9d8a6",
-        boxShadow: `0 2px 2px black`
-      }
-    }
-  }
+        boxShadow: `0 2px 2px black`,
+      },
+    },
+  },
 });
 const currentUser = {
-    userId: 1
-}
+  userId: 1,
+};
 
 export const createComment = () => {
   const [contentState, setContentState] = useState("");
-  const {onSave, currentUser} = props
+  const { onSave, currentUser } = props;
   const [error, setError] = useState("");
   function validate() {
     if (!contentState) {
@@ -35,20 +35,26 @@ export const createComment = () => {
   }
   // need to be able to keep values, i think comments could really useTransition as they are identical to comment except that they are even simpler
   return (
-    <Comment value={currentUser.userId} variant="primary" className="comment__card comment__card--create">
-        <section className="comment__card-left">
+    <Comment
+      value={currentUser.userId}
+      variant="primary"
+      className="comment__card comment__card--create"
+    >
+      <section className="comment__card-left">
         <h2>Create Comment</h2>
-    <form onSubmit={(event) => event.preventDefault()}>
-      <input
-        type="text"
-        name="content"
-        placeholder="What do you have to say for yourself Nerd?"
-        onChange={(event) => setContentState(event.target.value)}
-        value={contentState}
-      />
-    </form>
-        </section>
-    <Button variant="primary" onClick={() => validate()}>Post</Button>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <input
+            type="text"
+            name="content"
+            placeholder="What do you have to say for yourself Nerd?"
+            onChange={(event) => setContentState(event.target.value)}
+            value={contentState}
+          />
+        </form>
+      </section>
+      <Button variant="primary" onClick={() => validate()}>
+        Post
+      </Button>
     </Comment>
   );
 };
