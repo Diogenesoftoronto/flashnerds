002e4a1f535e97d65c8b595072d3af04e7039327
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-24 00:16:53 -0500

    error comments

--- app/src/components/comments/index.jsx
+++ app/src/components/comments/index.jsx
@@ -5,10 +5,19 @@ import ErrorComponent from "./Error";
 import Status from "./Status";
 // import CommentList from "./CommentList";
 import DisplayComments from "./DisplayComments";
-import { currentUser } from "../../context/useAuth";
-import { postComment, deleteComment } from "../../context/AppDataContext";
+// import { currentUser } from "../../context/useAuth";
+// import { postComment, deleteComment } from "../../context/AppDataContext";
 import ShowComment from "./Show";
 // define modes
+const currentUser = {
+  userId: 1,
+  name: "",
+  avatar: ""
+}
+
+// make axios request from 
+const deleteComment = (props) => setTimeout(console.log("deleteCOmment", props), 1000)
+const postComment = (props) => {setTimeout(console.log("postComment", props),  1000 )}
 const modes = {
   EMPTY: "EMPTY",
   SHOW: "SHOW",
@@ -26,16 +35,13 @@ const {
   SAVING,
   ERROR,
   EMPTY,
-  CONFIRM_DELETE,
-  CONFIRM_SAVE,
-  CONFIRM_EDIT,
   DELETE,
 } = modes;
 
 const Comment = (props) => {
   const { comment } = props;
   const { mode, transition, back } = useVisualMode(comment ? SHOW : EMPTY);
-  const { userId, content, post_time, flashcardId } = comment;
+  // const { userId, content, post_time, flashcardId } = comment;
   // create a save function
   //create delete function
   // create show, empty and saving components
@@ -78,7 +84,7 @@ const Comment = (props) => {
     <Status message="🥸 Your Nerdy comment is being Saved...Unfortunately 🥸" />
   );
 
-  const showComponent = <ShowComment {...comment} />;
+  const showComponent = <ShowComment comment={comment} user={currentUser}/>;
 
   const emptyComponent = <DisplayComments onClick={() => transition(SHOW)} />;
   return mode === SHOW
