995b2ac50f8a969d70d7a1105481f04059ba2333
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-24 02:42:06 -0500

    comment data change

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -15,25 +15,41 @@ const tags = [
     text: "tag3",
   },
 ];
+// const comments = [
+//   {
+//     id: 1,
+//     avatar: "https://i.imgur.com/LpaY82x.png",
+//     userName: "Bobby",
+//     content: "This is a comment.",
+//     likes: 20,
+//     timestamp: 1645483654553,
+//   },
+//   {
+//     id: 2,
+//     avatar: "https://i.imgur.com/LpaY82x.png",
+//     userName: "Bobby's Twin",
+//     content: "This is another comment.",
+//     likes: 200,
+//     timestamp: 1645483650000,
+//   },
+// ];
 const comments = [
-  {
-    id: 1,
-    avatar: "https://i.imgur.com/LpaY82x.png",
-    userName: "Bobby",
-    content: "This is a comment.",
-    likes: 20,
-    timestamp: 1645483654553,
-  },
-  {
-    id: 2,
-    avatar: "https://i.imgur.com/LpaY82x.png",
-    userName: "Bobby's Twin",
-    content: "This is another comment.",
-    likes: 200,
-    timestamp: 1645483650000,
-  },
-];
-
+    {
+      id: 1,
+      avatar: "https://i.imgur.com/LpaY82x.png",
+      name: "Bobby",
+      content: "This is a comment.",
+      likes: 20,
+      post_time: '2021-03-18 21:50:39'
+    },
+    {
+      id: 2,
+      name: "Bobby's Twin",
+      content: "This is another comment.",
+      likes: 200,
+      timestamp: '2021-03-05 02:29:08'
+    }
+  ];
 const question = "Question";
 const answer = "Answer";
 const likes = 0;
