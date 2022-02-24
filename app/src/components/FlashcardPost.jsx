dc4b28029b09be3cf3da6213a9e6976c64aa6bf3
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-02-23 21:11:20 -0500

    render comments instead of commentlist

--- app/src/components/FlashcardPost.jsx
+++ app/src/components/FlashcardPost.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "./Buttons/Button";
 import Flashcard from "./Flashcard";
-import CommentList from "./CommentList";
+import Comments from "./comments/Comments";
 import Likes from "./Likes";
 import { styled } from "@stitches/react";
 import { TagList } from "./tag/tagList";
@@ -48,7 +48,7 @@ function FlashcardPost(props) {
       <div className="flashcard-content">
       <Flashcard question={question} answer={answer} isFlipped={isFlipped} setIsFlipped={setIsFlipped}/>
       <TagList tags={tags}/>
-      <CommentList comments={comments}/>
+      <Comments comments={comments}/>
       <Likes likes={likes}/>
       </div>
       <Button variant='violet' onClick={() => nextCard()}>
@@ -57,7 +57,7 @@ function FlashcardPost(props) {
       </div>
     <footer>
       <Likes likes={likes}/>
-      <CommentList comments={comments}/>
+      <Comments comments={comments}/>
     </footer>
     </article>
   );
